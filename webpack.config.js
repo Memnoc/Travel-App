1c20cb7b17fba49389b9a15ca5055d917d735419
Memnoc <smartdroidesign@gmail.com>
2020-04-25 10:31:33 +0100

    chore: added html-webpack-plugin
    - html webpack plugin added
    - removed hardcoded reference to main.js in index.html (webpack does that now)
    - update the index.html file in server.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
@@ -10,6 +11,12 @@ module.exports = {
             exclude: /node_modules/,
             loader: "babel-loader"
         }]
-    }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        })
+    ]
 
 }
