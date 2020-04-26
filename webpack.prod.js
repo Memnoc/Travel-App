b82e6fa2dd6feddba1607075462515e9aa98e65e
Memnoc <smartdroidesign@gmail.com>
2020-04-26 09:23:28 +0100

    feat: added minicss extract plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,17 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
     module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
+        rules: [{
+            test: '/\.js$/',
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        }]
     },
     plugins: [
         new HtmlWebPackPlugin({
